ca3360d301c04970c3e25c502fed021662589bb3
Aleksandr Sorokin <alexandr.sor@gmail.com>
2026-05-07 11:55:13 +0000

    feat: default tags

--- demo-core/provider.tf
+++ demo-core/provider.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = var.project_region # Change to your preferred region
+
+  default_tags {
+    tags = var.common_tags
+  }
 }
--- demo-core/user.tf
+++ demo-core/user.tf
@@ -1,8 +1,8 @@
 resource "aws_iam_user" "tf_user" {
+  
   name = var.tf_user_name
   path = var.tf_user_path
 
-  tags = var.common_tags
 }
 
 
@@ -16,7 +16,6 @@ resource "aws_secretsmanager_secret" "tf_user_key_id" {
 
   kms_key_id = aws_kms_alias.tf_state.name
 
-  tags = var.common_tags
 }
 
 resource "aws_secretsmanager_secret_version" "tf_user_key_id" {
@@ -30,7 +29,6 @@ resource "aws_secretsmanager_secret" "tf_user_key_secret" {
 
   kms_key_id = aws_kms_alias.tf_state.name
 
-  tags = var.common_tags
 }
 
 resource "aws_secretsmanager_secret_version" "tf_user_key_secret" {
