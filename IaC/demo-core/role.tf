0ae493a6dc8b36d98664351e2daa74e5f1f37d77
AleksandrSor <58371538+AleksandrSor@users.noreply.github.com>
2026-05-19 22:50:06 +0800

    Feature/pipeline-2: add jobs to plan IaC changes against real environment on main PR (#19)
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * pipeline-2: add oidc provider for github and test aws credentials
    
    * feature/pipeline-2: add pipeline plan IaC change on main PR

--- IaC/demo-core/role.tf
+++ IaC/demo-core/role.tf
@@ -8,11 +8,39 @@ data "aws_iam_policy_document" "tf_execution_role_policy" {
       identifiers = concat(local.admin_user_arns, [aws_iam_user.tf_user.arn])
     }
   }
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${local.config.env.repository.name}:environment:${local.config.env.repository.protected_environment}"]
+    }
+  }
 }
 
 resource "aws_iam_role" "tf_execution_role" {
   name               = local.config.env.tf_role_name
   assume_role_policy = data.aws_iam_policy_document.tf_execution_role_policy.json
+}
+
+# Attach AdministratorAccess Policy to the Role
+# TODO: more restrictive permissions can be applied here based on the principle of least privilege
+resource "aws_iam_role_policy_attachment" "admin_attachment" {
+  role       = aws_iam_role.tf_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 data "aws_iam_policy_document" "tf_kms_user_role_policy" {
